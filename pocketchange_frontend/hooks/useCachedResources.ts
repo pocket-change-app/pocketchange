da896b36c94b59aafd1a022988c3eeb15df190cc
colecharb <40375497+colecharb@users.noreply.github.com>
2022-07-15 16:12:16 -0400

    added most fonts

--- pocketchange_frontend/hooks/useCachedResources.ts
+++ pocketchange_frontend/hooks/useCachedResources.ts
@@ -16,6 +16,18 @@ export default function useCachedResources() {
         await Font.loadAsync({
           ...FontAwesome.font,
           'space-mono': require('../assets/fonts/SpaceMono-Regular.ttf'),
+          'metropolis regular': require('../assets/fonts/Metropolis-Regular.otf'),
+          'metropolis regular italic': require('../assets/fonts/Metropolis-RegularItalic.otf'),
+          'metropolis medium': require('../assets/fonts/Metropolis-Medium.otf'),
+          'metropolis medium italic': require('../assets/fonts/Metropolis-MediumItalic.otf'),
+          'metropolis bold': require('../assets/fonts/Metropolis-Bold.otf'),
+          'metropolis bold italic': require('../assets/fonts/Metropolis-BoldItalic.otf'),
+          'metropolis semibold': require('../assets/fonts/Metropolis-SemiBold.otf'),
+          'metropolis semibold italic': require('../assets/fonts/Metropolis-SemiBoldItalic.otf'),
+          'metropolis extrabold': require('../assets/fonts/Metropolis-ExtraBold.otf'),
+          'metropolis extrabold italic': require('../assets/fonts/Metropolis-ExtraBoldItalic.otf'),
+          'metropolis black': require('../assets/fonts/Metropolis-Black.otf'),
+          'metropolis black italic': require('../assets/fonts/Metropolis-BlackItalic.otf'),
         });
       } catch (e) {
         // We might want to provide this error information to an error reporting service
