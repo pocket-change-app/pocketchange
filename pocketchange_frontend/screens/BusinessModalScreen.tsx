24b1183f0383c4daab65eddd1371edb12678b690
elias <elias.williams@mail.utoronto.ca>
2022-07-16 18:23:45 -0400

    business card images

--- pocketchange_frontend/screens/BusinessModalScreen.tsx
+++ pocketchange_frontend/screens/BusinessModalScreen.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, Image } from 'react-native';
+import { Platform, Image, Pressable } from 'react-native';
 
 import { styles } from '../Styles';
 import EditScreenInfo from '../components/EditScreenInfo';
@@ -10,22 +10,29 @@ export default function BusinessModalScreen({ route, navigation }: { route: any,
 
   const { name, address, pocket, imageURL } = route.params;
 
-
   return (
     <View style={styles.container}>
       
       <View style={ styles.card }>
-        <View style={ styles.imageContainer }>
+        <View style={ styles.businessHeaderImageContainer }>
           <Image
             style = { styles.businessHeaderImage } 
             source = { imageURL }
           />
         </View>
-        <View style={ styles.businessInfo }>
+        <View style={ styles.businessModalInfo }>
           <Text style={ styles.name }>{ name }</Text>
           <Text style={ styles.address }>{ address }</Text>
           <Text style={ styles.pocket }>{ pocket }</Text>
         </View>
+        <Pressable style={styles.payButton} >
+          <Text style={ styles.payButtonText }>PAY</Text>
+        </Pressable>
+      </View>
+
+      <View style={ [styles.card, styles.pocketChangeBalanceCard] }>
+        <Text style={ styles.pocketBig }>{ pocket } Change</Text>
+        <Text style={ styles.changeBig }>$8.94</Text>
       </View>
       
       {/* Use a light status bar on iOS to account for the black space above the modal */}
