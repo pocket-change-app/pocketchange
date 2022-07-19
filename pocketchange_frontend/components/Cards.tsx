62c70855afee26058b2bf28f36a47c485ea24296
colecharb <40375497+colecharb@users.noreply.github.com>
2022-07-18 21:13:30 -0400

    different size business names in small and large cards

--- pocketchange_frontend/components/Cards.tsx
+++ pocketchange_frontend/components/Cards.tsx
@@ -13,32 +13,32 @@ export function BusinessCardSm({ navigation, name, address, pocket, imageURL }: 
         pocket: pocket,
         imageURL: imageURL,
       })}
-      >
+    >
       <View style={[styles.card, styles.businessListItemCard]}>
-        
-        <View style={ styles.businessListImageContainer }>
+
+        <View style={styles.businessListImageContainer}>
           <Image
-            style = { styles.businessListImage } 
-            source = { imageURL }
+            style={styles.businessListImage}
+            source={imageURL}
           />
         </View>
 
-        <View style={ styles.businessListInfo }>
-          <Text style={styles.name}>{name}</Text>
+        <View style={styles.businessListInfo}>
+          <Text style={styles.businessNameSm}>{name}</Text>
           <Text style={styles.address}>{address}</Text>
           <Text style={styles.pocket}>{pocket}</Text>
         </View>
 
       </View>
 
-      </Pressable>
+    </Pressable>
   )
 }
 
 export function PocketCardSm({ name, imageURL }: { name: string, imageURL?: string }) {
   return (
     <View style={styles.card}>
-      <Text style={styles.name}>{name}</Text>
+      <Text style={styles.businessNameSm}>{name}</Text>
     </View>
   )
 }
@@ -46,9 +46,9 @@ export function PocketCardSm({ name, imageURL }: { name: string, imageURL?: stri
 export function IdCard({ name, imageURL, lifetimeChange, dateOfBirth }: { name: string, imageURL?: string, lifetimeChange: number, dateOfBirth: string }) {
   return (
     <View style={[styles.card, styles.idCard]}>
-      <Text style={styles.name}>{name}</Text>
-      <Text style={styles.name}>{lifetimeChange}</Text>
-      <Text style={styles.name}>{dateOfBirth}</Text>
+      <Text style={styles.businessNameSm}>{name}</Text>
+      <Text style={styles.businessNameSm}>{lifetimeChange}</Text>
+      <Text style={styles.businessNameSm}>{dateOfBirth}</Text>
     </View>
   )
 }
--- pocketchange_frontend/screens/BusinessModalScreen.tsx
+++ pocketchange_frontend/screens/BusinessModalScreen.tsx
@@ -12,29 +12,29 @@ export default function BusinessModalScreen({ route, navigation }: { route: any,
 
   return (
     <View style={styles.container}>
-      
-      <View style={ styles.card }>
-        <View style={ styles.businessHeaderImageContainer }>
+
+      <View style={styles.card}>
+        <View style={styles.businessHeaderImageContainer}>
           <Image
-            style = { styles.businessHeaderImage } 
-            source = { imageURL }
+            style={styles.businessHeaderImage}
+            source={imageURL}
           />
         </View>
-        <View style={ styles.businessModalInfo }>
-          <Text style={ styles.name }>{ name }</Text>
-          <Text style={ styles.address }>{ address }</Text>
-          <Text style={ styles.pocket }>{ pocket }</Text>
+        <View style={styles.businessModalInfo}>
+          <Text style={styles.businessNameLg}>{name}</Text>
+          <Text style={styles.address}>{address}</Text>
+          <Text style={styles.pocket}>{pocket}</Text>
         </View>
         <Pressable style={styles.payButton} >
-          <Text style={ styles.payButtonText }>PAY</Text>
+          <Text style={styles.payButtonText}>PAY</Text>
         </Pressable>
       </View>
 
-      <View style={ [styles.card, styles.pocketChangeBalanceCard] }>
-        <Text style={ styles.pocketBig }>{ pocket } Change</Text>
-        <Text style={ styles.changeBig }>$8.94</Text>
+      <View style={[styles.card, styles.pocketChangeBalanceCard]}>
+        <Text style={styles.pocketBig}>{pocket} Change</Text>
+        <Text style={styles.changeBig}>$8.94</Text>
       </View>
-      
+
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
