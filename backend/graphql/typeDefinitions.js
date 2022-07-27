6ae8a1c35816f39e89a87a34ef4eceecb35d476d
meconsens <micaela.consens@mail.utoronto.ca>
2022-07-27 16:24:38 -0400

    cleaning up duplicate paramater bugs and removing pocket manager leftover

--- backend/graphql/typeDefinitions.js
+++ backend/graphql/typeDefinitions.js
@@ -7,7 +7,6 @@ const ScalarTypeDefs = require('./schema/scalarTypeDefs')
 const BusinessTypeDefs = require('./schema/Business/typeDefinitions')
 const ChangeTypeDefs = require('./schema/Change/typeDefinitions')
 const PocketTypeDefs = require('./schema/Pocket/typeDefinitions')
-const PocketManagerTypeDefs = require('./schema/PocketManager/typeDefinitions')
 const TransactionTypeDefs = require('./schema/Transaction/typeDefinitions')
 const UserTypeDefs= require('./schema/User/typeDefinitions')
 
@@ -18,7 +17,6 @@ module.exports = mergeTypes([
   BusinessTypeDefs,
   ChangeTypeDefs,
   PocketTypeDefs,
-  PocketManagerTypeDefs,
   TransactionTypeDefs,
   UserTypeDefs
 
