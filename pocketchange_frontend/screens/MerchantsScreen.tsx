38dc41438c1344f4d4bc9bad6452fcd9704f92d3
colecharb <40375497+colecharb@users.noreply.github.com>
2022-07-29 15:34:49 -0400

    SEARCH BARS

--- pocketchange_frontend/screens/MerchantsScreen.tsx
+++ pocketchange_frontend/screens/MerchantsScreen.tsx
@@ -1,14 +1,18 @@
 import { ScrollView, FlatList } from 'react-native';
+import { SearchBar } from '@rneui/base';
 
 import { styles } from '../Styles';
 import { businesses } from '../dummy';
 import { BusinessCardSm } from '../components/Cards';
 import { Text, View } from '../components/Themed';
+import { Component } from 'react';
 
 const R = require('ramda');
 
-export default function MerchantsScreen({ navigation }: { navigation: any }) {
-
+export default class MerchantsScreen extends Component {
+  state = {
+    search: '',
+  }
 
 
   // const renderBusinessCard = (busId: string, name: string, address: string, pocket: string, imageURL: string) => (
@@ -22,41 +26,63 @@ export default function MerchantsScreen({ navigation }: { navigation: any }) {
   //   />
   // )
 
-  return (
-    <FlatList
-      style={styles.container}
+  updateSearch = (search) => {
+    this.setState({ search });
+  };
 
-      data={businesses}
-      renderItem={({ item, index, separators }) => (
-        <BusinessCardSm
-          key={item.busID}
-          navigation={navigation}
-          name={item.name}
-          address={item.address}
-          pocket={item.pocket}
-          imageURL={item.imageURL}
-          bio={item.bio}
-          people={item.people}
+  render() {
+    const { search } = this.state;
+
+    return (
+      <>
+        <SearchBar
+          containerStyle={styles.searchBarContainer}
+          inputContainerStyle={styles.searchBarInputContainer}
+          inputStyle={styles.searchBarInput}
+          round
+          placeholder="Search Merchants"
+          onChangeText={this.updateSearch}
+          value={search}
         />
-      )}
-    />
+        <FlatList
+          contentContainerStyle={styles.businessFlatList}
 
-    //   <ScrollView
-    //     style={styles.container}
-    //   >
-    //     {R.map(
-    //       ({ busID, name, address, pocket, imageURL }) => (
-    //         <BusinessCardSm
-    //           key={busID}
-    //           navigation={navigation}
-    //           name={name}
-    //           address={address}
-    //           pocket={pocket}
-    //           imageURL={imageURL}
-    //         />
-    //       ), businesses
-    //     )}
-    //   </ScrollView>
-    // );
-  )
+          data={businesses}
+          renderItem={({ item, index, separators }) => (
+
+            <BusinessCardSm
+              key={item.busID}
+              navigation={this.props.navigation}
+              name={item.name}
+              address={item.address}
+              pocket={item.pocket}
+              imageURL={item.imageURL}
+              bio={item.bio}
+              people={item.people}
+            />
+
+          )}
+        />
+
+      </>
+
+      //   <ScrollView
+      //     style={styles.container}
+      //   >
+      //     {R.map(
+      //       ({ busID, name, address, pocket, imageURL }) => (
+      //         <BusinessCardSm
+      //           key={busID}
+      //           navigation={navigation}
+      //           name={name}
+      //           address={address}
+      //           pocket={pocket}
+      //           imageURL={imageURL}
+      //         />
+      //       ), businesses
+      //     )}
+      //   </ScrollView>
+      // );
+    )
+  }
 }
