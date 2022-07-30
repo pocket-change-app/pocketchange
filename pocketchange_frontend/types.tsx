5b0e1687e82c34feb3f7dbb0dc239fbd0b3a157b
colecharb <40375497+colecharb@users.noreply.github.com>
2022-07-30 09:18:03 -0400

    new page types

--- pocketchange_frontend/types.tsx
+++ pocketchange_frontend/types.tsx
@@ -17,7 +17,7 @@ declare global {
 export type RootStackParamList = {
   Root: NavigatorScreenParams<RootTabParamList> | undefined;
   BusinessModal: undefined;
-  // PocketScreen: undefined;
+  PocketScreen: undefined;
   NotFound: undefined;
 };
 
