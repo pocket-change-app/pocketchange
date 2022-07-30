92c6dff62febed7e78baa8ed8f0e27b5e66a7473
colecharb <40375497+colecharb@users.noreply.github.com>
2022-07-30 08:56:25 -0400

    pocket-specific screen

---
+++ pocketchange_frontend/screens/PocketScreen.tsx
@@ -0,0 +1,83 @@
+import { FlatList } from 'react-native';
+import { SearchBar } from '@rneui/base';
+
+import { styles } from '../Styles';
+import { businesses } from '../dummy';
+import { ScreenContainer } from '../components/Themed';
+import { BusinessCardSm } from '../components/Cards';
+import { Text, View } from '../components/Themed';
+import { Component } from 'react';
+
+const R = require('ramda');
+
+export default class PocketScreen extends Component {
+
+  // props: route, navigation
+
+  state = {
+    search: '',
+  }
+
+  renderBusinessCard = ({ item, index, separators }: { item: any, index: any, separators: any }) => (
+
+    <BusinessCardSm
+      key={item.busID}
+      navigation={this.props.navigation}
+      name={item.name}
+      address={item.address}
+      pocket={item.pocket}
+      imageURL={item.imageURL}
+      bio={item.bio}
+      people={item.people}
+    />
+
+  )
+
+  updateSearch = (search: string) => {
+    this.setState({ search });
+  };
+
+  render() {
+    const { search } = this.state;
+
+    return (
+      <>
+        <SearchBar
+          containerStyle={styles.searchBarContainer}
+          inputContainerStyle={styles.searchBarInputContainer}
+          inputStyle={styles.searchBarInput}
+          round
+          placeholder="Search Merchants"
+          onChangeText={this.updateSearch}
+          value={search}
+        />
+        <ScreenContainer>
+          <FlatList
+            contentContainerStyle={styles.businessFlatList}
+
+            data={businesses}
+            renderItem={this.renderBusinessCard}
+          />
+        </ScreenContainer>
+      </>
+
+      //   <ScrollView
+      //     style={styles.container}
+      //   >
+      //     {R.map(
+      //       ({ busID, name, address, pocket, imageURL }) => (
+      //         <BusinessCardSm
+      //           key={busID}
+      //           navigation={navigation}
+      //           name={name}
+      //           address={address}
+      //           pocket={pocket}
+      //           imageURL={imageURL}
+      //         />
+      //       ), businesses
+      //     )}
+      //   </ScrollView>
+      // );
+    )
+  }
+}
