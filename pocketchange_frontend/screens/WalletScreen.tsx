b6d3ac8d93db9d353368b8e9e741346fd84e5055
colecharb <40375497+colecharb@users.noreply.github.com>
2022-07-31 11:36:49 -0400

    WalletScreen has a ScreenContainer now

--- pocketchange_frontend/screens/WalletScreen.tsx
+++ pocketchange_frontend/screens/WalletScreen.tsx
@@ -1,24 +1,26 @@
 import { ScrollView } from 'react-native';
 
 import { styles } from '../Styles';
-import { Text, View } from '../components/Themed';
+import { ScreenContainer, Text, View } from '../components/Themed';
 import gold from '../constants/Colors';
 import { BalancesCard, IdCard } from '../components/Cards';
 import { user } from '../dummy';
 
 export default function WalletScreen() {
   return (
-    <ScrollView style={styles.container}>
-      <IdCard
-        name={user.name}
-        lifetimeChange={user.lifetimeChange}
-        dateOfBirth={user.dateOfBirth}
-        imageURL={user.imageURL}
-      />
-      <BalancesCard
-        changeTotal={user.changeTotal}
-        topPockets={user.topPockets}
-      />
-    </ScrollView>
+    <ScreenContainer>
+      <ScrollView style={styles.container}>
+        <IdCard
+          name={user.name}
+          lifetimeChange={user.lifetimeChange}
+          dateOfBirth={user.dateOfBirth}
+          imageURL={user.imageURL}
+        />
+        <BalancesCard
+          changeTotal={user.changeTotal}
+          topPockets={user.topPockets}
+        />
+      </ScrollView>
+    </ScreenContainer>
   );
 }
