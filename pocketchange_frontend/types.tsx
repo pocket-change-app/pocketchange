179f4cd22e2087ef874f6e037078843d4111d801
colecharb <40375497+colecharb@users.noreply.github.com>
2022-08-02 14:53:55 -0400

    ConsumerTransaction in RootStackParamList type

--- pocketchange_frontend/types.tsx
+++ pocketchange_frontend/types.tsx
@@ -21,6 +21,7 @@ export type RootStackParamList = {
   PocketScreen: undefined;
   NotFound: undefined;
   TransactionModal: undefined;
+  ConsumerTransaction: undefined;
 };
 
 export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
