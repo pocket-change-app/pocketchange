ef2ca8b561c9099e320a8be3a8ccb2ccfef6d2cc
colecharb <40375497+colecharb@users.noreply.github.com>
2022-08-02 16:19:56 -0400

    PayAmountScreen layout, styling

--- pocketchange_frontend/screens/PayAmountScreen.tsx
+++ pocketchange_frontend/screens/PayAmountScreen.tsx
@@ -1,15 +1,35 @@
 import { styles } from "../Styles";
 import { View, Text } from '../components/Themed'
-import { PayAmountCard } from '../components/Cards'
+import { ButtonWithText, PayAmountCard } from '../components/Cards'
 
 export default function PayAmountScreen({ route, navigation }: { route: any, navigation: any }) {
 
   const { busID, name, address, pocket, imageURL } = route.params;
 
+  const amount = '2.50' // dummy val for consumer entered amount
+
   return (
     <View style={styles.container}>
-      <Text>hiya</Text>
-      {/* <PayAmountCard /> */}
+      <PayAmountCard
+        name={name}
+        address={address}
+        pocket={pocket}
+        imageURL={imageURL}
+        navigation={navigation}
+      />
+
+      <ButtonWithText
+        text={'Next'}
+        onPress={() => navigation.navigate("PayTip", {
+          // navigation: navigation,
+          busID: busID,
+          name: name,
+          address: address,
+          pocket: pocket,
+          imageURL: imageURL,
+          amount: amount
+        })}
+      />
     </View>
   )
 }
