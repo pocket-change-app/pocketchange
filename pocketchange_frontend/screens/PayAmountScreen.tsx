8083648fc6d6f70b223f7b08ce090e5d7182ec8b
colecharb <40375497+colecharb@users.noreply.github.com>
2022-08-03 01:03:16 -0400

    pulled PayAmountCard inside PayAmountScreen

--- pocketchange_frontend/screens/PayAmountScreen.tsx
+++ pocketchange_frontend/screens/PayAmountScreen.tsx
@@ -1,22 +1,47 @@
 import { styles } from "../Styles";
 import { View, Text } from '../components/Themed'
-import { ButtonWithText, PayAmountCard } from '../components/Cards'
+import { CardHeader, HorizontalLine, ButtonWithText, PayAmountCard } from '../components/Cards'
+import { TextInput } from "react-native";
+import { useState } from "react";
 
 export default function PayAmountScreen({ route, navigation }: { route: any, navigation: any }) {
 
   const { busID, name, address, pocket, imageURL } = route.params;
 
-  const amount = '2.50' // dummy val for consumer entered amount
+  const [amount, onChangeAmount] = useState(null)
 
   return (
     <View style={styles.container}>
-      <PayAmountCard
+      {/* <PayAmountCard
         name={name}
         address={address}
         pocket={pocket}
         imageURL={imageURL}
         navigation={navigation}
-      />
+      /> */}
+
+      <View style={styles.card}>
+        <CardHeader text='Pay' />
+
+        <View style={{ flexDirection: 'row' }}>
+          <View style={styles.businessListInfo}>
+            <Text style={styles.businessNameSm}>{name}</Text>
+            <Text style={styles.address}>{address}</Text>
+            <Text style={styles.pocket}>{pocket}</Text>
+          </View>
+        </View>
+
+        <HorizontalLine />
+
+        <View style={styles.container}>
+          <TextInput
+            keyboardType='numeric'
+            value={'$' + amount}
+            onChangeText={onChangeAmount}
+            placeholder="$"
+          />
+        </View>
+      </View>
 
       <ButtonWithText
         text={'Next'}
@@ -30,6 +55,6 @@ export default function PayAmountScreen({ route, navigation }: { route: any, nav
           amount: amount,
         })}
       />
-    </View>
+    </View >
   )
 }
