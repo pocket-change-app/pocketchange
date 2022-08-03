d3cc01b5a1c448a112e8c934016e65ccdf7e1add
colecharb <40375497+colecharb@users.noreply.github.com>
2022-08-02 21:46:53 -0400

    added PaySummaryScreen

---
+++ pocketchange_frontend/screens/PaySummaryScreen.tsx
@@ -0,0 +1,36 @@
+import { styles } from "../Styles";
+import { View, Text } from '../components/Themed'
+import { ButtonWithText, PaySummaryCard } from '../components/Cards'
+
+export default function PaySummaryScreen({ route, navigation }: { route: any, navigation: any }) {
+
+  const { busID, name, address, pocket, imageURL, amount, tip } = route.params;
+
+  // const amount = '2.50' // dummy val for consumer entered amount
+
+  return (
+    <View style={styles.container}>
+      <PaySummaryCard
+        name={name}
+        address={address}
+        pocket={pocket}
+        imageURL={imageURL}
+        amount={amount}
+        tip={tip}
+      />
+
+      <ButtonWithText
+        text={'Confirm'}
+        onPress={() => navigation.navigate("PayConfirmation", {
+          // navigation: navigation,
+          busID: busID,
+          name: name,
+          address: address,
+          pocket: pocket,
+          imageURL: imageURL,
+          amount: amount,
+        })}
+      />
+    </View>
+  )
+}
