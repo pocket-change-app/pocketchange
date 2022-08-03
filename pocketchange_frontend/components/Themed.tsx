28416c1328e873e21c6176c319a10ceb5cc60426
colecharb <40375497+colecharb@users.noreply.github.com>
2022-08-03 13:53:57 -0400

    moved Lines to their own file

--- pocketchange_frontend/components/Themed.tsx
+++ pocketchange_frontend/components/Themed.tsx
@@ -9,7 +9,7 @@ import { Text as DefaultText, View as DefaultView } from 'react-native';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
-import { HorizontalLine } from './Cards';
+import { HorizontalLine } from './Lines';
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
