4e75a6e487f9eec0c8efaf0210857a9d3e7834da
colecharb <40375497+colecharb@users.noreply.github.com>
2022-08-02 23:54:00 -0400

    nav style

--- pocketchange_frontend/Styles.tsx
+++ pocketchange_frontend/Styles.tsx
@@ -15,8 +15,8 @@ export const styles = StyleSheet.create({
   navigationHeader: {
     height: 100,
     backgroundColor: colors.bg,
-    // borderBottomWidth: 2,
-    borderWidth: 0
+    borderBottomWidth: 0,
+    // borderWidth: 0,
   },
   navigationHeaderTitle: {
     fontSize: 30,
@@ -26,8 +26,8 @@ export const styles = StyleSheet.create({
   tabBar: {
     height: 100,
     backgroundColor: colors.bg,
-    // borderTopWidth: BORDER_WIDTH,
-    borderWidth: 0,
+    borderTopWidth: 0,
+    // borderWidth: 0,
     borderColor: colors.light,
   },
 
