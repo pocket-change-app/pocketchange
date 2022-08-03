5ae5a13eaafe5a0b4f7e91bf1dc035db83295a01
colecharb <40375497+colecharb@users.noreply.github.com>
2022-08-03 01:02:26 -0400

    removed unnecessary whitespace

--- pocketchange_frontend/screens/PayTabScreen.tsx
+++ pocketchange_frontend/screens/PayTabScreen.tsx
@@ -47,7 +47,6 @@ export default function PayTabScreen({ navigation }: { navigation: any }) {
       <ScreenContainer>
         <FlatList
           contentContainerStyle={styles.businessFlatList}
-
           data={businesses}
           renderItem={renderBusinessCard}
         />
