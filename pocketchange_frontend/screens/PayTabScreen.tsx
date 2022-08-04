c0b0f2468ae86e8d833f42a7f7b2f94a0361a484
colecharb <40375497+colecharb@users.noreply.github.com>
2022-08-04 11:30:27 -0400

    Navigate to Merchant instead of BusinessModal

--- pocketchange_frontend/screens/PayTabScreen.tsx
+++ pocketchange_frontend/screens/PayTabScreen.tsx
@@ -50,7 +50,7 @@ export default function PayTabScreen({ navigation }: { navigation: any }) {
             <>
               <View style={styles.cardHeader}><Text style={styles.cardHeaderText}>Suggested</Text></View>
               <Pressable
-                onPress={() => navigation.navigate('BusinessModal', {
+                onPress={() => navigation.navigate('Merchant', {
                   business: businesses[0]
                 })}
               >
