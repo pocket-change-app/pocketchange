9bae9c4e5e7f29b7b4facd0d64e04a98d4e98f00
colecharb <40375497+colecharb@users.noreply.github.com>
2022-08-08 21:09:02 -0400

    added ScreenContainers

--- pocketchange_frontend/screens/PaySummaryScreen.tsx
+++ pocketchange_frontend/screens/PaySummaryScreen.tsx
@@ -1,5 +1,5 @@
 import { styles } from "../Styles";
-import { View, Text } from '../components/Themed'
+import { View, Text, ScreenContainer } from '../components/Themed'
 import { ButtonWithText, PaySummaryCard } from '../components/Cards'
 
 export default function PaySummaryScreen({ route, navigation }: { route: any, navigation: any }) {
@@ -9,28 +9,30 @@ export default function PaySummaryScreen({ route, navigation }: { route: any, na
   // const amount = '2.50' // dummy val for consumer entered amount
 
   return (
-    <View style={styles.container}>
-      <PaySummaryCard
-        name={name}
-        address={address}
-        pocket={pocket}
-        imageURL={imageURL}
-        amount={amount}
-        tip={tip}
-      />
+    <ScreenContainer>
+      <View style={styles.container}>
+        <PaySummaryCard
+          name={name}
+          address={address}
+          pocket={pocket}
+          imageURL={imageURL}
+          amount={amount}
+          tip={tip}
+        />
 
-      <ButtonWithText
-        text={'Confirm'}
-        onPress={() => navigation.navigate("PayConfirmation", {
-          // navigation: navigation,
-          busID: busID,
-          name: name,
-          address: address,
-          pocket: pocket,
-          imageURL: imageURL,
-          amount: amount,
-        })}
-      />
-    </View>
+        <ButtonWithText
+          text={'Confirm'}
+          onPress={() => navigation.navigate("PayConfirmation", {
+            // navigation: navigation,
+            busID: busID,
+            name: name,
+            address: address,
+            pocket: pocket,
+            imageURL: imageURL,
+            amount: amount,
+          })}
+        />
+      </View>
+    </ScreenContainer>
   )
 }
