3be709a6eb2fc3911c1a2700eb4b6a8bdd424b99
colecharb <40375497+colecharb@users.noreply.github.com>
2022-08-21 16:27:42 -0400

    payment overhaul

--- pocketchange_frontend/screens/PayAmountScreen.tsx
+++ pocketchange_frontend/screens/PayAmountScreen.tsx
@@ -2,9 +2,10 @@ import { styles } from "../Styles";
 import { View, Text, ScreenContainer } from '../components/Themed'
 import { CardHeader, ButtonWithText, PayAmountCard } from '../components/Cards'
 import { HorizontalLine } from "../components/Lines";
-import { TextInput } from "react-native";
+import { KeyboardAvoidingView, Platform, TextInput } from "react-native";
 import { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
+import { colors } from "../constants/Colors";
 
 
 export default function PayAmountScreen({ route, navigation }: { route: any, navigation: any }) {
@@ -13,53 +14,68 @@ export default function PayAmountScreen({ route, navigation }: { route: any, nav
 
   const { busID, name, address, pocket, imageURL } = business;
 
-  const [amount, onChangeAmount] = useState(null)
+  const [amount, setAmount] = useState('')
+
+  function onChangeAmount(amt: string) {
+    setAmount(amt)
+  }
 
   return (
-    <ScreenContainer>
-      <View style={styles.container}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? 'padding' : "height"}
+      keyboardVerticalOffset={100}
+      style={{ flex: 1 }}
+    >
+      <ScreenContainer>
+        <View style={[styles.container, { flex: 1, justifyContent: 'center' }]}>
 
-        <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+          <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
 
-        {/* <PayAmountCard
+          {/* <PayAmountCard
         name={name}
         address={address}
         pocket={pocket}
         imageURL={imageURL}
         navigation={navigation}
       /> */}
+          <View>
+            <View style={styles.card}>
+              {/* <CardHeader text='Pay' /> */}
 
-        <View style={styles.card}>
-          {/* <CardHeader text='Pay' /> */}
+              <View style={{ flexDirection: 'row' }}>
+                <View style={styles.businessListInfo}>
+                  <Text style={styles.businessNameSm}>{name}</Text>
+                  <Text style={styles.address}>{address}</Text>
+                  <Text style={styles.pocket}>{pocket}</Text>
+                </View>
+              </View>
 
-          <View style={{ flexDirection: 'row' }}>
-            <View style={styles.businessListInfo}>
-              <Text style={styles.businessNameSm}>{name}</Text>
-              <Text style={styles.address}>{address}</Text>
-              <Text style={styles.pocket}>{pocket}</Text>
+              <HorizontalLine />
+
+              <View style={styles.inputContainer}>
+                <Text style={styles.paymentInputText}>$</Text>
+                <TextInput
+                  style={styles.paymentInputText}
+                  keyboardType='numeric'
+                  value={amount}
+                  onChangeText={onChangeAmount}
+                  placeholder={'0.00'}
+                  placeholderTextColor={colors.light}
+                />
+              </View>
             </View>
-          </View>
 
-          <HorizontalLine />
-
-          <View style={styles.container}>
-            <TextInput
-              keyboardType='numeric'
-              value={'$' + amount}
-              onChangeText={onChangeAmount}
-              placeholder="$"
+            <ButtonWithText
+              text={'Next'}
+              onPress={() => navigation.navigate("PayTip", {
+                navigation: navigation,
+                business: business,
+                amount: parseFloat(amount).toFixed(2),
+              })}
             />
           </View>
         </View>
-
-        <ButtonWithText
-          text={'Next'}
-          onPress={() => navigation.navigate("PayTip", {
-            navigation: navigation,
-            business: business,
-          })}
-        />
-      </View>
-    </ScreenContainer>
+      </ScreenContainer>
+    </KeyboardAvoidingView>
   )
 }
