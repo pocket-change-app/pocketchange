4269194a7d96744b063eba57244d1285084cdc70
colecharb <40375497+colecharb@users.noreply.github.com>
2022-08-21 16:27:42 -0400

    payment overhaul

--- pocketchange_frontend/screens/PaySummaryScreen.tsx
+++ pocketchange_frontend/screens/PaySummaryScreen.tsx
@@ -1,24 +1,51 @@
-import { styles } from "../Styles";
+import { MARGIN, styles } from "../Styles";
 import { View, Text, ScreenContainer } from '../components/Themed'
 import { ButtonWithText, PaySummaryCard } from '../components/Cards'
+import { HorizontalLine } from "../components/Lines";
+import { colors } from "../constants/Colors";
 
 export default function PaySummaryScreen({ route, navigation }: { route: any, navigation: any }) {
 
   const { busID, name, address, pocket, imageURL, amount, tip } = route.params;
 
-  // const amount = '2.50' // dummy val for consumer entered amount
+  const amountNum = parseFloat(amount)
+  const tipNum = parseFloat(tip)
 
   return (
     <ScreenContainer>
       <View style={styles.container}>
-        <PaySummaryCard
-          name={name}
-          address={address}
-          pocket={pocket}
-          imageURL={imageURL}
-          amount={amount}
-          tip={tip}
-        />
+        <View style={styles.card}>
+          {/* <CardHeader text='Summary' /> */}
+
+          <View style={{ flexDirection: 'row' }}>
+            <View style={styles.businessListInfo}>
+              <Text style={styles.businessNameSm}>{name}</Text>
+              <Text style={styles.address}>{address}</Text>
+              <Text style={styles.pocket}>{pocket}</Text>
+            </View>
+          </View>
+
+          <HorizontalLine />
+
+          <View style={styles.container}>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <Text style={[styles.paymentSummaryText, { textAlign: 'left' }]}>Subtotal</Text>
+              <Text style={[styles.paymentSummaryText, { textAlign: 'right' }]}>{amount}</Text>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <Text style={[styles.paymentSummaryText, { textAlign: 'left' }]}>Tip</Text>
+              <Text style={[styles.paymentSummaryText, { textAlign: 'right' }]}>{tip}</Text>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <Text style={[styles.paymentSummaryText, { textAlign: 'left' }]}>Change Used</Text>
+              <Text style={[styles.paymentSummaryText, { textAlign: 'right' }]}>-{'2.63'}</Text>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: MARGIN }}>
+              <Text style={[styles.paymentSummaryText, { textAlign: 'left' }]}>Total</Text>
+              <Text style={[styles.paymentSummaryText, { textAlign: 'right' }]}>${(amountNum + tipNum - 2.63).toFixed(2)}</Text>
+            </View>
+          </View>
+        </View>
 
         <ButtonWithText
           text={'Confirm'}
--- pocketchange_frontend/screens/PayTipScreen.tsx
+++ pocketchange_frontend/screens/PayTipScreen.tsx
@@ -1,7 +1,10 @@
-import { styles } from "../Styles";
+import { MARGIN, styles } from "../Styles";
 import { View, Text, ScreenContainer } from '../components/Themed'
-import { ButtonWithText, PayAmountCardSm, PayTipCard } from '../components/Cards'
-import { TimePickerAndroid } from "react-native";
+import { ButtonWithText, PayAmountCardSm, CardHeader, PayTipCard } from '../components/Cards'
+import { KeyboardAvoidingView, TextInput, TimePickerAndroid } from "react-native";
+import { HorizontalLine } from "../components/Lines";
+import { useState } from "react";
+import { colors } from "../constants/Colors";
 
 export default function PayTipScreen({ route, navigation }: { route: any, navigation: any }) {
 
@@ -9,37 +12,90 @@ export default function PayTipScreen({ route, navigation }: { route: any, naviga
 
   const { busID, name, address, pocket, imageURL } = business;
 
-  const tip = 6.20
+  const [percentage, setPercentage] = useState('20')
+  const [tip, setTip] = useState((amount * parseFloat(percentage) / 100).toString())
+
+  function updateTipByPercentage(p: string) {
+    setPercentage(p)
+    setTip((p == '') ? '0.00' : (parseFloat(p) / 100 * amount).toFixed(2))
+  }
+
+  function updateTipByAmount(t: string) {
+    setTip(t)
+    setPercentage((t == '') ? '0.0' : (parseFloat(t) / amount * 100).toFixed(0))
+  }
 
   return (
-    <ScreenContainer>
-      <View style={styles.container}>
-        <PayAmountCardSm
-          name={name}
-          address={address}
-          pocket={pocket}
-          imageURL={imageURL}
-          amount={amount}
-        />
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? 'padding' : "height"}
+      keyboardVerticalOffset={100}
+      style={{ flex: 1 }}
+    >
+      <ScreenContainer>
+        <View style={[styles.container, { flex: 1, justifyContent: 'center' }]}>
+          <View>
+            <View style={styles.card}>
+              {/* <CardHeader text='Pay' /> */}
 
-        <PayTipCard
-          amount={amount}
-        />
+              <View style={{ flexDirection: 'row' }}>
+                <View style={styles.businessListInfo}>
+                  <Text style={styles.businessNameSm}>{name}</Text>
+                  <Text style={styles.address}>{address}</Text>
+                  <Text style={styles.pocket}>{pocket}</Text>
+                </View>
+              </View>
 
-        <ButtonWithText
-          text={'Next'}
-          onPress={() => navigation.navigate("PaySummary", {
-            // navigation: navigation,
-            busID: busID,
-            name: name,
-            address: address,
-            pocket: pocket,
-            imageURL: imageURL,
-            amount: amount,
-            tip: tip,
-          })}
-        />
-      </View>
-    </ScreenContainer>
+              <HorizontalLine />
+
+              <View style={styles.container}>
+                <Text style={[styles.paymentInputText, { textAlign: 'center', color: colors.gold }]}>${amount}</Text>
+              </View>
+            </View>
+
+            <View style={styles.card}>
+              <CardHeader text='Tip' />
+
+              <View style={[styles.inputContainer, { flexDirection: 'row', justifyContent: 'center' }]}>
+
+                <TextInput
+                  keyboardType='numeric'
+                  style={styles.paymentInputText}
+                  value={percentage.toString()}
+                  onChangeText={updateTipByPercentage}
+                />
+                <Text style={styles.paymentInputText}>%</Text>
+
+                <Text style={styles.paymentInputText}>  =  </Text>
+
+                <Text style={styles.paymentInputText}>$</Text>
+                <TextInput
+                  keyboardType='numeric'
+                  style={styles.paymentInputText}
+                  value={tip.toString()}
+                  onChangeText={updateTipByAmount}
+                />
+
+              </View>
+
+              {/* <HorizontalLine /> */}
+            </View>
+
+            <ButtonWithText
+              text={'Next'}
+              onPress={() => navigation.navigate("PaySummary", {
+                // navigation: navigation,
+                busID: busID,
+                name: name,
+                address: address,
+                pocket: pocket,
+                imageURL: imageURL,
+                amount: parseFloat(amount).toFixed(2),
+                tip: parseFloat(tip).toFixed(2),
+              })}
+            />
+          </View>
+        </View>
+      </ScreenContainer>
+    </KeyboardAvoidingView>
   )
 }
