b187f62bb0f902f9c6a5b2adf1eaa33b9b2ad059
colecharb <40375497+colecharb@users.noreply.github.com>
2022-08-31 18:20:39 -0400

    SettingsAboutScreen defined

---
+++ pocketchange_frontend/screens/SettingsAboutScreen.tsx
@@ -0,0 +1,35 @@
+import { ScrollView } from "react-native";
+import { DivHeader, SettingPressable } from "../components/Cards";
+import { HorizontalLine } from "../components/Lines";
+import { ScreenContainer, View } from "../components/Themed";
+import { styles } from "../Styles";
+
+export default function SettingsAboutScreen({ route, navigation }: { route: any, navigation: any }) {
+  return (
+    <ScreenContainer>
+      <ScrollView
+        contentContainerStyle={[styles.container]}
+      >
+
+        <DivHeader text="Documents" />
+
+        <View style={styles.card}>
+          <SettingPressable
+            iconName='bell-concierge'
+            settingText={"Terms of Service"}
+          // onPress={navigation.navigate('ViewPDF', {
+          //   title: "Terms of Service",
+          //   document: require('../documents/Terms_of_Service.pdf')
+          // })}
+          />
+          <HorizontalLine />
+          <SettingPressable
+            iconName='privacy'
+            settingText={"Privacy Policy"}
+          />
+        </View>
+
+      </ScrollView>
+    </ScreenContainer >
+  )
+}
