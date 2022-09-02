a4480b2453e056a6720640fb4a39f54d4054f509
meconsens <micaela.consens@mail.utoronto.ca>
2022-09-02 17:55:27 -0400

    add hooks to export

--- pocketchange_frontend/hooks-apollo/index.js
+++ pocketchange_frontend/hooks-apollo/index.js
@@ -1,7 +1,15 @@
+import useGetBusinessQuery from './Business/useGetBusinessQuery'
 import useGetAllBusinessesQuery from './Business/useGetAllBusinessesQuery'
 import useGetLovedBusinessesQuery from './Business/useGetLovedBusinessesQuery'
+import useGetAllTransactionsQuery from './Transaction/useGetAllTransactionsQuery'
+import useGetAllPocketsQuery from './Pocket/useGetAllPocketsQuery'
+import usePocketQuery from './Pocket/usePocketQuery'
 
 export {
+    useGetBusinessQuery,
     useGetAllBusinessesQuery,
-    useGetLovedBusinessesQuery
+    useGetLovedBusinessesQuery,
+    useGetAllTransactionsQuery,
+    useGetAllPocketsQuery,
+    usePocketQuery,
 }
