8a91d442f63704994e6f63c8a0b4e0e65638a500
meconsens <micaela.consens@mail.utoronto.ca>
2022-10-03 14:45:24 -0400

    sql competition schema

---
+++ backend/databases/SQLSchema/Competition.model.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, Sequelize) => {
+    Pocket = require("./Pocket.model.js")(sequelize, Sequelize);
+    const Competition = sequelize.define("competition", {
+        competitionID: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true
+        },
+        prizeValue:{
+            type: Sequelize.DECIMAL(19,4),
+            allowNull: false
+        },
+        pocketID: {
+            type: Sequelize.UUID,
+            allowNull: false,
+            references: {
+                model: Pocket,
+                key: 'pocketID'
+            }
+        },
+        startDate: {
+            type: Sequelize.DATE,
+            allowNull: false
+        },
+        endDate: {
+            type: Sequelize.DATE,
+            allowNull: false
+        }
+    });
+  
+    return Competition;
+};
