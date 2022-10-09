fa5a4aba2c116ec3708de60362242a916522b010
colecharb <40375497+colecharb@users.noreply.github.com>
2022-10-09 17:24:14 -0400

    foxed comment for clarity

--- backend/databases/mongoSchema/mongoCompetition.js
+++ backend/databases/mongoSchema/mongoCompetition.js
@@ -3,15 +3,14 @@ const mongoose = require('mongoose')
 const mongoCompetitionSchema = new mongoose.Schema({
     competitionID: { type: mongoose.Schema.Types.String }, // no auto since generated in SQL
     competitionName: { type: mongoose.Schema.Types.String },
-    description:{type: mongoose.Schema.Types.String}, //
+    description: { type: mongoose.Schema.Types.String }, //
     status:{
-      pending: {type:mongoose.Schema.Types.Boolean}, //if the pocket has yet to be approved
-      approved: {type:mongoose.Schema.Types.Boolean}, //if the pocket has been approved & is active
-      deactivated: {type:mongoose.Schema.Types.Boolean} //if the pocket is still active
-  },
-    //georegion?
-  })
-  
-  const mongoCompetition = mongoose.model('mongoCompetition', mongoCompetitionSchema)
+      pending: { type:mongoose.Schema.Types.Boolean }, //if the competition has yet to be approved
+      approved: { type:mongoose.Schema.Types.Boolean }, //if the competition has been approved & is active
+      deactivated: { type:mongoose.Schema.Types.Boolean } //if the competition is still active
+    },
+})
 
-  module.exports ={mongoCompetition}
+const mongoCompetition = mongoose.model('mongoCompetition', mongoCompetitionSchema)
+
+module.exports = { mongoCompetition }
