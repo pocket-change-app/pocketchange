c63f9cb87983310da8f218bbc7becff8381f7d20
colecharb <40375497+colecharb@users.noreply.github.com>
2022-10-09 17:18:20 -0400

    added snapItUp competition to dummy

--- pocketchange_frontend/dummy.tsx
+++ pocketchange_frontend/dummy.tsx
@@ -3,6 +3,24 @@
 import { RoleLevel, RoleType } from "./contexts/Auth";
 
 
+//// COMPETITIONS ////
+
+export const snapItUp = {
+  competitionID: '000',
+  competitionName: 'Snap it UP!',
+  description: "To enter once, ask for the QR code and Snap it UP after making a purchase in any Uptown Yonge business. Each month, one randomly chosen participant will win $500 in Uptown Yonge Change! Limited to one entry per day.",
+  startDate: '05-05-2022',
+  endDate: '12-12-2022',
+  prizeValue: 500.00,
+  participants: [
+    { name: 'Danny' },
+    { name: 'Ava' },
+    { name: 'Elrond' },
+    { name: 'Adar' },
+  ]
+}
+
+
 //// FOR CONSUMER SIDE ////
 
 export const user = {
