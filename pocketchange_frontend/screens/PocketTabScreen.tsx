690e281932e74a3eb5d1f87705db2db5c2da1fa0
colecharb <40375497+colecharb@users.noreply.github.com>
2022-10-14 20:45:45 -0400

    search pockets AND merchants :)

--- pocketchange_frontend/screens/PocketTabScreen.tsx
+++ pocketchange_frontend/screens/PocketTabScreen.tsx
@@ -2,41 +2,84 @@ import { SafeAreaView, FlatList, ScrollView, Dimensions, KeyboardAvoidingView, P
 import { SearchBar } from '@rneui/base';
 
 import { styles, MARGIN, POCKET_CARD_SCREEN_MARGIN } from '../Styles';
-import { pockets } from '../dummy';
-import { PocketListCard, PocketListSeparator, PocketSearchResult } from "../components/Cards";
+import { BusinessCardSm, PocketListCard, PocketListSeparator, PocketSearchResult } from "../components/Cards";
 import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
 import { ScreenContainer } from '../components/Themed';
 import React, { useContext, useState } from 'react';
 import { colors } from '../constants/Colors';
 import { AuthContext } from '../contexts/Auth';
 import { useQuery } from '@apollo/client';
 import PocketQueries from '../hooks-apollo/Pocket/queries'
+import { useGetAllBusinessesQuery, useGetAllPocketsQuery } from '../hooks-apollo';
 
-const R = require('ramda');
+// const R = require('ramda');
 
 
 export default function PocketTabScreen({ navigation, route }: { navigation: any, route: any }) {
 
-  const authContext = useContext(AuthContext); 
+  const authContext = useContext(AuthContext);
+
+  const { allBusinesses, loading } = useGetAllBusinessesQuery(null)
 
   const [searchQuery, setSearchQuery] = useState('')
-  const [pocketSearchResults, setPocketSearchResults] = useState()
-
-  const updateSearch = (text: string) => {
-    setSearchQuery(text)
-    setPocketSearchResults(() => {
-      const formattedQuery = text.toLowerCase().trim()
-      const results = pocketData.getAllPockets.filter(p => p.pocketName.toLowerCase().includes(formattedQuery))
-      return results
-    })
-  };
 
 
-  const { data: pocketData, loading: pocketLoading, error: pocketError } = useQuery(PocketQueries.getAllPockets, { variables: { } });
+  const { data: pocketData, loading: pocketsLoading, error: pocketError } = useQuery(PocketQueries.getAllPockets, { variables: {} });
   if (pocketError) return <Text>{pocketError.message}</Text>;
-  if (pocketLoading) return <ActivityIndicator size="large" color={colors.subtle} style={{margin: 10}}/>
-  
+  if (pocketsLoading) return <ActivityIndicator size="large" color={colors.subtle} style={{ margin: 10 }} />
+
+  const pocketSearchResults = pocketData.getAllPockets.filter(
+    p => p.pocketName.toLowerCase().includes(
+      searchQuery.toLowerCase().trim()
+    )
+  )
+
+  const businessSearchResults = allBusinesses.filter(
+    b => b.businessName.toLowerCase().includes(
+      searchQuery.toLowerCase().trim()
+    )
+  )
+
+  let allSearchResults = []
+  for (var i in pocketSearchResults) {
+    const p = pocketSearchResults[i]
+    allSearchResults.push(
+      p
+    )
+  }
+  for (var i in businessSearchResults) {
+    const b = businessSearchResults[i]
+    allSearchResults.push(
+      b
+    )
+  }
+
+  // console.log(allSearchResults);
+
+
+  const renderSearchResult = ({ item, index, separators }: any) => {
+    switch (item.__typename) {
+      case "Pocket":
+        return (
+          <PocketSearchResult
+            navigation={navigation}
+            pocket={item} />
+        )
+      case "Business":
+        return (
+          <BusinessCardSm
+            navigation={navigation}
+            business={item}
+          />
+        )
+      default:
+        // return (
+        //   <Text> UNSUPPORTED OBJECT </Text>
+        // )
+        return
+    }
+  }
+  console.log(' ');
  
   function PageContents() {
     if (searchQuery == '') {
@@ -71,15 +114,8 @@ export default function PocketTabScreen({ navigation, route }: { navigation: any
 
           // ItemSeparatorComponent={PocketListSeparator}
 
-          data={pocketSearchResults}
-          renderItem={({ item, index, separators }) => (
-            <PocketSearchResult
-              key={item.pocketID}
-              navigation={navigation}
-              pocket={item}
-            />
-          )
-          }
+          data={allSearchResults}
+          renderItem={renderSearchResult}
         />
       )
     }
@@ -101,10 +137,10 @@ export default function PocketTabScreen({ navigation, route }: { navigation: any
         inputContainerStyle={styles.searchBarInputContainer}
         inputStyle={styles.searchBarInput}
         // round
-        placeholder="Search Pockets"
+        placeholder="Search Pockets and Merchants"
         placeholderTextColor={colors.subtle}
 
-        onChangeText={updateSearch}
+        onChangeText={setSearchQuery}
         value={searchQuery}
       />
 
