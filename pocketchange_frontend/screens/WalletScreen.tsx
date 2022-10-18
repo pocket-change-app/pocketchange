48634b385f967cd4cd57d095c935b8d6e340c404
colecharb <40375497+colecharb@users.noreply.github.com>
2022-10-18 12:41:50 -0400

    pull to refresh wallet

--- pocketchange_frontend/screens/WalletScreen.tsx
+++ pocketchange_frontend/screens/WalletScreen.tsx
@@ -1,11 +1,11 @@
-import { ActivityIndicator, ScrollView } from 'react-native';
+import { ActivityIndicator, RefreshControl, ScrollView } from 'react-native';
 
 import { MARGIN, styles } from '../Styles';
 import { ScreenContainer, Text, View } from '../components/Themed';
 import { BalancesCard, IdCard, HistoryCard } from '../components/Cards';
 import { user } from '../dummy';
 import { useGetAllTransactionsQuery } from '../hooks-apollo';
-import { useContext } from 'react';
+import { useCallback, useContext, useState } from 'react';
 import { AuthContext } from '../contexts/Auth';
 const R = require('ramda');
 
@@ -13,14 +13,30 @@ import ChangeBalanceQueries from '../hooks-apollo/ChangeBalance/queries'
 import QRScanQueries from '../hooks-apollo/QRScan/queries'
 import { colors } from '../constants/Colors';
 import useGetAllChangeBalancesQuery from '../hooks-apollo/ChangeBalance/useGetAllChangeBalancesQuery';
+import wait, { waitTimes } from '../utils/wait';
 
 export default function WalletScreen({ navigation }: { navigation: any }) {
 
   const authContext = useContext(AuthContext); 
   
   const userID = authContext.userFirebase.uid;
-  const { allTransactions, loading: transactionLoading, error: transactionError } =  useGetAllTransactionsQuery(undefined, undefined, userID, undefined, undefined);
-  const { data: changeBalanceData, loading: changeBalanceLoading, error: changeBalanceError, refetch: refetchChangeBalances } = useGetAllChangeBalancesQuery(userID, undefined);
+
+  const transactionsQuery = useGetAllTransactionsQuery(undefined, undefined, userID, undefined, undefined)
+  const changeBalanceQuery = useGetAllChangeBalancesQuery(userID, undefined)
+
+  const { allTransactions, loading: transactionLoading, error: transactionError, refetch: refetchTransactions } = transactionsQuery
+  const { data: changeBalanceData, loading: changeBalanceLoading, error: changeBalanceError, refetch: refetchChangeBalances } = changeBalanceQuery
+
+  const [refreshing, setRefreshing] = useState(false)
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    Promise.all([
+      wait(waitTimes.RefreshScreen),
+      refetchChangeBalances,
+      refetchTransactions,
+    ]).then(() => setRefreshing(false));
+  }, []);
 
   if (transactionError) return(<Text>{transactionError.message}</Text>);
   if (changeBalanceError) return(<Text>{changeBalanceError.message}</Text>);
@@ -28,6 +44,12 @@ export default function WalletScreen({ navigation }: { navigation: any }) {
   return (
     <ScreenContainer>
       <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }
         style={styles.container}
         nestedScrollEnabled={false}>
 
