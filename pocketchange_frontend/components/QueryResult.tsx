c1d2712c1a3d21911bb2247f065f59c9235250c2
colecharb <40375497+colecharb@users.noreply.github.com>
2022-10-26 22:27:25 -0400

    added optional indicattorSize param to QueryResult
    It takes either 'small' or 'large'.
    Did some reformatting while I was here

--- pocketchange_frontend/components/QueryResult.tsx
+++ pocketchange_frontend/components/QueryResult.tsx
@@ -2,19 +2,39 @@ import React from "react";
 import { ActivityIndicator, Text } from "react-native";
 import { colors } from "../constants/Colors";
 
-export const QueryResult = ({loading, error, data, children}: any) => {
-    if (error) {
-      return <Text>ERROR: {error.message}</Text>;
-    }
-    if (loading) {
-      return (
-        <ActivityIndicator size="large" color={colors.subtle} style={{ margin: 10 }} />
-      );
-    }
-    if (!data) {
-      return <Text>Nothing to show...</Text>;
-    }
-    if (data) {
-      return children;
-    }
-  };
+export const QueryResult = (
+  {
+    loading,
+    error,
+    data,
+    children,
+    indicatorSize = 'large',
+  }: {
+    loading: boolean,
+    error: any,
+    data: any,
+    children: any,
+    indicatorSize?: number | 'small' | 'large' | undefined,
+  }
+) => {
+  if (error) {
+    return (
+      <Text>
+        ERROR: {error.message}
+      </Text>
+    )
+  }
+  if (loading) {
+    return (
+      <ActivityIndicator
+        size={indicatorSize}
+        color={colors.subtle}
+      />
+    );
+  }
+  if (data) {
+    return children;
+  } else {
+    return <Text>Nothing to show...</Text>;
+  }
+};
