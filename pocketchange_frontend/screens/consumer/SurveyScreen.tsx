44696bc0fa88641df92b8561532d4aec8fa98e83
colecharb <40375497+colecharb@users.noreply.github.com>
2022-11-01 17:02:36 -0400

    big improvements to surveyScreen
    including a router for displaying the appropriate survey component

--- pocketchange_frontend/screens/consumer/SurveyScreen.tsx
+++ pocketchange_frontend/screens/consumer/SurveyScreen.tsx
@@ -1,17 +1,39 @@
 import { NavigationState, Route, RouteProp, RouterConfigOptions } from "@react-navigation/native";
+import { useState } from "react";
 import { ButtonWithText } from "../../components/Cards";
+import { ChoiceSurvey } from "../../components/Surveys";
 import { View } from "../../components/Themed";
 import { colors } from "../../constants/Colors";
+import { surveyType, dummyChoiceSurvey } from "../../dummy";
 import { styles } from "../../Styles";
 
 
 export default function SurveyScreen({ route, navigation }: { route: any, navigation: any }) {
+
+  const { survey } = route.params
+
+  const [surveyFilled, setSurveyFilled] = useState(false)
+
+  const Survey = ({ survey, onSubmit }: { survey: any, onSubmit: () => any }) => {
+
+    switch (survey.type) {
+      case surveyType.choice:
+        return (
+          <ChoiceSurvey
+            survey={survey}
+            onSubmit={onSubmit}
+          />
+        )
+      default:
+        return (null)
+    }
+  }
+
   return (
-    <View style={[styles.screenContainer, styles.container, { justifyContent: 'center' }]}>
-      <ButtonWithText
-        text="ayoooooo"
-        onPress={() => navigation.goBack()}
-        color={colors.tomato}
+    <View style={[styles.screenContainer, styles.container, { justifyContent: 'center', backgroundColor: colors.overlay }]}>
+      <Survey
+        survey={survey}
+        onSubmit={() => navigation.goBack()}
       />
     </View>
   )
