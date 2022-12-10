7aa390a43c851c40e633978beac62d8b5820d3c4
colecharb <40375497+colecharb@users.noreply.github.com>
2022-12-09 22:33:49 -0500

    addition: pins for businesses!!!
    with custom callouts that navigate to the business screen

--- pocketchange_frontend/screens/consumer/MapScreen.tsx
+++ pocketchange_frontend/screens/consumer/MapScreen.tsx
@@ -1,8 +1,12 @@
 import { LocationObject } from "expo-location";
 import { useContext } from "react";
-import MapView from "react-native-maps";
+import MapView, { Callout, Marker } from "react-native-maps";
+import { BusinessCardSm, BusinessInfo } from "../../components/Cards";
 import { View } from "../../components/Themed";
+import { colors } from "../../constants/Colors";
 import { AuthContext } from "../../contexts/Auth";
+import { useGetAllBusinessesQuery } from "../../hooks-apollo";
+import { styles } from "../../Styles";
 
 
 
@@ -11,13 +15,16 @@ export default function MapScreen({ route, navigation }: { route: any, navigatio
 
   const authContext = useContext(AuthContext)
 
+  const { data: businessesData, loading: businessesLoading, error: businessesError, refetch: refetchBusinesses } = useGetAllBusinessesQuery()
+
   // const authContext = useContext(AuthContext)
   console.log(JSON.stringify(authContext.location, null, '  '));
 
   return (
     // <View style={{ flex: 1 }}>
     <MapView
-      style={{ flex: 1 }}
+      // provider='google'
+      style={[styles.image, styles.pocketListImage, { flex: 1, borderRadius: 0 }]}
       showsUserLocation
       showsMyLocationButton
       initialRegion={{
@@ -27,7 +34,26 @@ export default function MapScreen({ route, navigation }: { route: any, navigatio
         longitudeDelta: 0.05,
       }}
       mapType='mutedStandard'
-    />
+    >
+      {/* BUSINESS MARKERS */}
+      {businessesData?.getAllBusinesses?.map((business: any) => (
+        <Marker
+          key={business?.businessID}
+          coordinate={{ latitude: business.latitude, longitude: business.longitude }}
+          // title={business?.businessName}
+          // description={business?.description}
+          pinColor={colors.gold}
+        >
+          <Callout >
+            <BusinessInfo
+              navigation={navigation}
+              businessID={business.businessID}
+            />
+          </Callout>
+        </Marker>
+      ))}
+
+    </MapView>
     // </View>
   )
 }
