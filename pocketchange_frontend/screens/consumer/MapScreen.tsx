5c3195cced6553c8d1bb004561427ddd151a9af7
colecharb <40375497+colecharb@users.noreply.github.com>
2022-12-26 12:48:34 -0500

    polygons working!
    test polygon is in. just need pocket outline data

--- pocketchange_frontend/screens/consumer/MapScreen.tsx
+++ pocketchange_frontend/screens/consumer/MapScreen.tsx
@@ -1,13 +1,13 @@
 import { LocationObject } from "expo-location";
 import { useContext } from "react";
-import MapView, { Callout, Marker } from "react-native-maps";
+import MapView, { Callout, Details, Marker, Polygon, Polyline, Region, Geojson } from "react-native-maps";
 import { BusinessCardSm, BusinessInfo } from "../../components/Cards";
-import { View } from "../../components/Themed";
+import { Text, View } from "../../components/Themed";
 import { colors } from "../../constants/Colors";
 import { AuthContext } from "../../contexts/Auth";
 import { useGetAllBusinessesQuery } from "../../hooks-apollo";
 import { styles } from "../../Styles";
-
+import polylabel from "polylabel";
 
 
 
@@ -18,7 +18,50 @@ export default function MapScreen({ route, navigation }: { route: any, navigatio
   const { data: businessesData, loading: businessesLoading, error: businessesError, refetch: refetchBusinesses } = useGetAllBusinessesQuery()
 
   // const authContext = useContext(AuthContext)
-  console.log(JSON.stringify(authContext.location, null, '  '));
+  // console.log(JSON.stringify(authContext.location, null, '  '));
+
+  const onRegionChange = (region: Region, details: Details) => {
+    console.log(`latitude: ${region.latitude}`);
+    console.log(`longitude: ${region.longitude}`);
+    console.log();
+  }
+
+  const testPolygon = [
+    {
+      latitude: 43.672308614364816,
+      longitude: -79.31499755441902
+    },
+    {
+      latitude: 43.66487422523087,
+      longitude: -79.33416422648763
+    },
+    {
+      latitude: 43.64462602965331,
+      longitude: -79.33303587058124
+    },
+    {
+      latitude: 43.64961756176936,
+      longitude: -79.31525808779296
+    },
+    {
+      latitude: 43.65852428179872,
+      longitude: -79.31099125836472
+    },
+    {
+      latitude: 43.662446240880364,
+      longitude: -79.29470252644401
+    },
+  ]
+
+  const p1 = [testPolygon.map(coord => [coord.latitude, coord.longitude])]
+
+  const labelCoords = polylabel(p1, 1.0)
+  const testLabelLatLng = {
+    latitude: labelCoords[0],
+    longitude: labelCoords[1]
+  }
+  console.log(testLabelLatLng);
+
 
   return (
     // <View style={{ flex: 1 }}>
@@ -27,6 +70,7 @@ export default function MapScreen({ route, navigation }: { route: any, navigatio
       style={[styles.image, styles.pocketListImage, { flex: 1, borderRadius: 0 }]}
       showsUserLocation
       showsMyLocationButton
+      // onRegionChange={onRegionChange}
       initialRegion={{
         latitude: 43.66393648913529,
         longitude: -79.3154142212031,
@@ -44,7 +88,7 @@ export default function MapScreen({ route, navigation }: { route: any, navigatio
           // description={business?.description}
           pinColor={colors.gold}
         >
-          <Callout >
+          <Callout>
             <BusinessInfo
               navigation={navigation}
               businessID={business.businessID}
@@ -53,7 +97,26 @@ export default function MapScreen({ route, navigation }: { route: any, navigatio
         </Marker>
       ))}
 
+      {/* POLYGONS */}
+
+      <Polygon
+        coordinates={testPolygon}
+        // strokeWidth={2}
+        strokeColor={colors.gold}
+        fillColor={colors.gold + '22'}
+      >
+
+      </Polygon>
+      <Marker
+        key="asdf"
+        coordinate={testLabelLatLng}
+        pinColor={colors.tomato}
+      >
+        <Text style={[styles.pocket, { color: colors.gold }]}>
+          ~Experience Land~
+        </Text>
+      </Marker>
+
     </MapView>
-    // </View>
   )
 }
