ffdda93f2eee953eb7bfe49b6b45e103c63edf2f
colecharb <40375497+colecharb@users.noreply.github.com>
2022-12-28 14:45:08 -0500

    change: reorganization, added refetch

--- pocketchange_frontend/screens/merchant/TransactionsTabScreen.tsx
+++ pocketchange_frontend/screens/merchant/TransactionsTabScreen.tsx
@@ -1,4 +1,4 @@
-import { FlatList, KeyboardAvoidingView, RefreshControl } from 'react-native';
+import { FlatList, KeyboardAvoidingView, Platform, RefreshControl, SectionList } from 'react-native';
 import SearchBar from '../../components/SearchBar';
 import { useHeaderHeight } from '@react-navigation/elements'
 import { useCallback, useContext, useState } from 'react';
@@ -18,58 +18,54 @@ export default function TransactionsTabScreen({ navigation }: { navigation: any 
   const authContext = useContext(AuthContext);
   const businessID = authContext.activeRole.entityID
 
+
+
+  const [searchQuery, setSearchQuery] = useState('')
+
+  const [refreshing, setRefreshing] = useState(false)
+
   const { data: transactionsData, error: transactionsError, loading: transactionsLoading, refetch: refetchTransactions } = useGetAllTransactionsQuery(undefined, businessID)
   if (transactionsError) return (<Text>{transactionsError.message}</Text>)
 
-
-  const [refreshing, setRefreshing] = useState(false)
+  const searchResults = transactionsData?.getAllTransactions
 
   const onRefresh = useCallback(() => {
     setRefreshing(true);
     Promise.all([
       wait(waitTimes.RefreshScreen),
-      // refetchTransactions // once query is in
+      refetchTransactions // once query is in
     ]).then(() => setRefreshing(false));
   }, []);
 
-  // TODO: remove hard coded value, get logged in business
-  const business = {
-    businessID: '2b',
-    businessName: 'La Paella',
-    dateEstablished: '2015-01-01',
-    phoneNumber: '416-000-000',
-    website: 'paella.ca',
-    businessType: 'restaurant',
-    businessSubtype: 'spanish',
-    emailAddress: 'paella@gmail.com',
-    address: {
-      streetName: 'Queen St E',
-      buildingNumber: '1146',
-      unitNumber: '',
-      city: 'Toronto',
-      region: 'ON',
-      postalCode: 'M4M 1L1',
-    },
-    latitude: 43.6625197,
-    longitude: -79.336471,
-    businessTags: [],
-    stripeID: '2b',
-    description: 'La Paella was originally a Spanish catering company in the GTA that was started in 2010 by partners, Gabriel and Angel.  They decided to open a storefront location in Leslieville in 2017.',
-    deactivated: false,
-  };
+  // // TODO: remove hard coded value, get logged in business
+  // const business = {
+  //   businessID: '2b',
+  //   businessName: 'La Paella',
+  //   dateEstablished: '2015-01-01',
+  //   phoneNumber: '416-000-000',
+  //   website: 'paella.ca',
+  //   businessType: 'restaurant',
+  //   businessSubtype: 'spanish',
+  //   emailAddress: 'paella@gmail.com',
+  //   address: {
+  //     streetName: 'Queen St E',
+  //     buildingNumber: '1146',
+  //     unitNumber: '',
+  //     city: 'Toronto',
+  //     region: 'ON',
+  //     postalCode: 'M4M 1L1',
+  //   },
+  //   latitude: 43.6625197,
+  //   longitude: -79.336471,
+  //   businessTags: [],
+  //   stripeID: '2b',
+  //   description: 'La Paella was originally a Spanish catering company in the GTA that was started in 2010 by partners, Gabriel and Angel.  They decided to open a storefront location in Leslieville in 2017.',
+  //   deactivated: false,
+  // };
 
 
 
-  const [searchQuery, setSearchQuery] = useState('')
 
-  const searchResults = transactionsData?.getAllTransactions?.filter(
-    t => {
-      return true
-      // t.userName.toLowerCase().includes(
-      //   searchQuery.toLowerCase().trim()
-      // )
-    }
-  )
 
   // if(R.isNil(allTransactions) ){
   //   return null
@@ -80,7 +76,7 @@ export default function TransactionsTabScreen({ navigation }: { navigation: any 
 
   const renderTransactionCard = ({ item, index, separators }: { item: any, index: any, separators: any }) => {
 
-    console.log('rendering transaxtion');
+    // console.log('rendering transaction');
 
     return (
       <TranactionCardSm
