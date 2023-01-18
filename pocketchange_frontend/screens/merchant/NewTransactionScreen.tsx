76e7f50c4a3aa1f616a8c2f26f986bc264ac88ac
colecharb <40375497+colecharb@users.noreply.github.com>
2023-01-18 17:43:39 -0500

    addition: QR GENERATION IS A GO

--- pocketchange_frontend/screens/merchant/NewTransactionScreen.tsx
+++ pocketchange_frontend/screens/merchant/NewTransactionScreen.tsx
@@ -1,28 +1,48 @@
 import { MARGIN, styles } from "../../Styles";
+import Layout from "../../constants/Layout";
 import { View, Text, ScreenContainer } from '../../components/Themed'
 import { CardHeader, ButtonWithText, BusinessCardSm, BusinessInfo, } from '../../components/Cards'
 import { HorizontalLine } from "../../components/Lines";
-import { KeyboardAvoidingView, Platform, SafeAreaView, TextInput } from "react-native";
+import { Keyboard, KeyboardAvoidingView, Platform, SafeAreaView, TextInput } from "react-native";
 import { useContext, useState } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { colors } from "../../constants/Colors";
 import { AuthContext } from "../../contexts/Auth";
 import { useHeaderHeight } from '@react-navigation/elements'
 import { useBusinessQuery } from "../../hooks-apollo";
+import QRCode from "react-native-qrcode-svg";
 
 
 export default function NewTransactionScreen({ route, navigation }: { route: any, navigation: any }) {
 
   const authContext = useContext(AuthContext);
+  const businessID = authContext.activeRole.entityID;
 
-  const businessID = authContext.activeRole.entityID
+  const [amount, setAmount] = useState<string>('');
+  const [keyboardIsOpen, setKeyboardIsOpen] = useState<boolean>(true);
 
-  const [amount, setAmount] = useState<string>('')
+  const QR_SIZE = Layout.window.width * 2 / 3
+  const qrImage = require('../../assets/images/icon_blackout.png')
 
-  const amountValid = (parseFloat(amount) > 0)
-
-  // note: for testing only
+  // url for QR code
   const url = `https://www.pocketchangeapp.ca/open/tip/${businessID}/${parseFloat(amount).toFixed(2)}`
+
+  const amountValid = (parseFloat(amount) > 0);
+
+  const keyboardShowListener = Keyboard.addListener(
+    'keyboardWillShow',
+    () => {
+      setKeyboardIsOpen(true);
+    }
+  );
+  const keyboardHideListener = Keyboard.addListener(
+    'keyboardWillHide',
+    () => {
+      setKeyboardIsOpen(false);
+    }
+  );
+
+
 
   return (
 
@@ -66,24 +86,38 @@ export default function NewTransactionScreen({ route, navigation }: { route: any
               </View>
             </View>
 
-            <ButtonWithText
-              text={'Generate QR Code'}
-              color={amountValid ? colors.gold : undefined}
-              onPress={() => {
-                if (amountValid) {
-                  null
-                }
-              }}
-            />
 
-
+            {keyboardIsOpen ? (
+              <ButtonWithText
+                text={'Show QR Code'}
+                color={amountValid ? colors.gold : undefined}
+                onPress={() => {
+                  if (amountValid) {
+                    setAmount(parseFloat(amount).toFixed(2))
+                    Keyboard.dismiss()
+                  }
+                }}
+              />
+            ) : (
+              <View style={{ alignItems: 'center' }}>
+                <View style={[styles.card, styles.container]}>
+                  <QRCode
+                    size={QR_SIZE}
+                    value={url}
+                    logo={qrImage}
+                    logoSize={QR_SIZE * 7 / 33 + 1}
+                  />
+                </View>
+              </View>
+            )
+            }
 
 
           </View>
 
-          <Text>
+          {/* <Text>
             {url}
-          </Text>
+          </Text> */}
 
         </View>
 
