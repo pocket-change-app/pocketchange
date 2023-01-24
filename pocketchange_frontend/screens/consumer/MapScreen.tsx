287ad702feec9e2cdd0e7984ab19676597fed879
colecharb <40375497+colecharb@users.noreply.github.com>
2023-01-23 21:10:44 -0500

    Revert "removed unneeded styles"
    This reverts commit dc9fc203e014449c294a0a13031011c5df02084b.

--- pocketchange_frontend/screens/consumer/MapScreen.tsx
+++ pocketchange_frontend/screens/consumer/MapScreen.tsx
@@ -72,7 +72,7 @@ export default function MapScreen({ route, navigation }: { route: any, navigatio
     // <View style={{ flex: 1 }}>
     <MapView
       // provider='google'
-      style={{ flex: 1, borderRadius: 0 }}
+      style={[styles.image, styles.pocketListImage, { flex: 1, borderRadius: 0 }]}
       showsUserLocation
       showsMyLocationButton
       initialCamera={{
