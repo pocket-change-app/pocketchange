7285bf3e1ac0827a6349a13a57deda54653acd75
colecharb <40375497+colecharb@users.noreply.github.com>
2023-02-03 22:14:47 -0500

    added basic search (no api yet)

--- pocketchange_frontend/screens/consumer/BusinessCreationSearchScreen.tsx
+++ pocketchange_frontend/screens/consumer/BusinessCreationSearchScreen.tsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
+import { TextInput } from "react-native";
 import { ButtonWithText } from "../../components/Cards";
+import SearchBar from "../../components/SearchBar";
 import { ScreenContainer, Text, View } from "../../components/Themed";
+import { colors } from "../../constants/Colors";
 import { styles } from "../../Styles";
+import Constants from "expo-constants";
+
+
 
 export default function ({ route, navigation }: { route: any, navigation: any }) {
+
+  const [searchQuery, setSearchQuery] = useState<string>('')
+  // const [results, setResults] = useState<{}>()
+
+
+  const goToNextScreen = () => {
+    navigation.navigate('BusinessCreationEdit', {})
+  };
+
+
   return (
     <ScreenContainer>
       <View style={styles.container}>
 
+        <SearchBar
+          value={searchQuery}
+          onChangeText={setSearchQuery}
+          placeholder='Search for your business...'
+        />
+
+
         <ButtonWithText
           text='Next'
-          onPress={() => navigation.navigate('BusinessCreationEdit', {})}
+          onPress={goToNextScreen}
         />
 
       </View>
